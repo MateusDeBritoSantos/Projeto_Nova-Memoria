ae7587a1b9f10a6bed9aefee5d3697a21ca95e15
isaac_do_Golzin <isaacferreirapeixoto9@gmail.com>
2026-06-01 15:51:58 -0300

    mudancas para fazer o deploy

--- database.php
+++ database.php
@@ -1,14 +1,21 @@
 <?php
 
+//  CONFIG LOCAL (XAMPP)
 $server = "localhost";
 $server_user = "root";
 $server_password = "";
 $database_name = "nova_mem";
 
-// Conectar ao banco de dados
+//  SOBRESCREVE SE ESTIVER NO RENDER/RAILWAY
+$server = getenv("MYSQLHOST") ?: $server;
+$server_user = getenv("MYSQLUSER") ?: $server_user;
+$server_password = getenv("MYSQLPASSWORD") ?: $server_password;
+$database_name = getenv("MYSQLDATABASE") ?: $database_name;
+
+//  CONEXÃO
 $conexao = mysqli_connect($server, $server_user, $server_password, $database_name);
 
-// Verificar conexão
+//  ERRO DE CONEXÃO
 if (!$conexao) {
     die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
 }
